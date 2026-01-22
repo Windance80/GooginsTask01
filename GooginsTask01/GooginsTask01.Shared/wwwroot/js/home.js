945697de0b948f00d2357c194f0ebbabe99aa9ed
sandy <sandy.theodorus@gmail.com>
2026-01-22 12:27:58 +0700

    avoid invoking js duplicate in after first render for performance

--- GooginsTask01/GooginsTask01.Shared/wwwroot/js/home.js
+++ GooginsTask01/GooginsTask01.Shared/wwwroot/js/home.js
@@ -18,26 +18,8 @@ window.addCallyChangeListener = (calendarElement, dotNetHelper) => {
         // Optional: dispatch custom event or store value in dotNet object
         // You can also call back to C# here if needed
     });
-};
 
-window.addFabListener = (dotNetHelper) => {
-    window.addEventListener('resize', async () => {
-        const container = document.querySelector('#my-main-layout');
-        const fab = document.querySelector('#my-fab');
-
-        if (!container || !fab) return;
-
-        const rect = container.getBoundingClientRect();
-        const right = window.innerWidth - rect.right;
-
-        // console.log('resize');
-        // console.log("right: " + parseInt(right, 10));
-
-        return right.toString();
-
-        // fab.style.right = `${right + 16}px`; // 16px = 1rem
-    });
-
+    // get the right end of window for FAB anchor
     const container = document.querySelector('#my-main-layout');
     const fab = document.querySelector('#my-fab');
 
@@ -53,4 +35,21 @@ window.addFabListener = (dotNetHelper) => {
     // console.log("right: " + parseInt(right, 10));
 
     return right.toString();
+};
+
+window.addFabListener = (dotNetHelper) => {
+    window.addEventListener('resize', async () => {
+        const container = document.querySelector('#my-main-layout');
+        const fab = document.querySelector('#my-fab');
+
+        if (!container || !fab) return;
+
+        const rect = container.getBoundingClientRect();
+        const right = window.innerWidth - rect.right;
+
+        // console.log('resize');
+        // console.log("right: " + parseInt(right, 10));
+
+        return right.toString();        
+    });
 };
